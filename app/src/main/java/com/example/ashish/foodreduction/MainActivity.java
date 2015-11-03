0141e5b7c5c79dcb5ed99b03dd77efa456feefaf
Ashish Surana <ashishsurana@ymail.com>
2015-11-04 02:56:36 +0530

    Image Implemented

--- app/src/main/java/com/example/ashish/foodreduction/MainActivity.java
+++ app/src/main/java/com/example/ashish/foodreduction/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.ashish.foodreduction;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.media.Image;
+import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +14,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
@@ -19,11 +23,13 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    final Activity activity = this;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -31,38 +37,64 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         this.getWindow().requestFeature(Window.FEATURE_PROGRESS);
         setContentView(R.layout.activity_main);
-        WebView webView = (WebView) findViewById(R.id.webView);
+        WebView webView =  (WebView) findViewById(R.id.webView);
+//        WebView webView = new WebView(this); //(WebView) findViewById(R.id.webView);
         final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
         progressBar.setVisibility(View.VISIBLE);
         progressBar.setMax(100);
         webView.getSettings().setJavaScriptEnabled(true);
+//        webView.loadUrl("http://foodreduction.tk");
+        webView.loadUrl("http://facebook.com");
+        webView.canGoBackOrForward(5);
 
-        webView.setWebChromeClient(new WebChromeClient(){
+
+        webView.setWebChromeClient(new WebChromeClient() {
             @Override
             public void onProgressChanged(WebView view, int newProgress) {
-                if(progressBar.getVisibility()==View.GONE){
+                if (progressBar.getVisibility() == View.GONE) {
                     progressBar.setVisibility(View.VISIBLE);
-                    progressBar.setProgress(newProgress);
                 }
-                if (newProgress == 100){
+                progressBar.setProgress(newProgress);
+                if (newProgress == 100) {
                     progressBar.setVisibility(View.GONE);
                 }
             }
+
         });
 
         webView.setWebViewClient(new WebViewClient() {
 
-
+            @Override
+            public void onPageFinished(WebView view, String url) {
+//                setContentView(R.layout.activity_main);
+            }
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
                 return true;
             }
         });
+        Log.d("Status", String.valueOf(check_connectivity()));
+//        ImageView imageView = new ImageView(getApplicationContext());
+//        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+//        imageView.setLayoutParams(layoutParams);
+//        imageView.setImageResource(R.drawable.food);
+//        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativelayout);
+//        relativeLayout.setBackgroundResource(R.drawable.food);
 
-        webView.loadUrl("http://foodreduction.tk");
+
+
+    }//end of oncreate
+
+    public Boolean check_connectivity(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getActiveNetworkInfo()!=null
+                && connectivityManager.getActiveNetworkInfo().isAvailable()
+                && connectivityManager.getActiveNetworkInfo().isConnected()){
+            return true;
+        }
+        return false;
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
